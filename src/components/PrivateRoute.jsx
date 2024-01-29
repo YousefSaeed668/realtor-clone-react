d0db1ad2a6487a672036eaa9232257e5ec6c04c2
yousef saeed <yousefsaeed668@gmail.com>
2024-01-29 17:32:39 +0200

    create private route for protecting the profile page and add the logout functionality

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from "react-router";
+import { useAuthStatus } from "../hooks/useAuthStatus";
+
+function PrivateRoute() {
+  const { loggedIn, checkingStatus } = useAuthStatus();
+
+  if (checkingStatus) return <h1>Loading...</h1>;
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
+}
+
+export default PrivateRoute;
