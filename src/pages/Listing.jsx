d1a9fd272e13ed71b18856a3a84ba8f8577f2718
yousef saeed <yousefsaeed668@gmail.com>
2024-01-31 20:30:55 +0200

    add share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -2,14 +2,7 @@ import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { db } from "../firebase";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import SwiperCore, {
-//   EffectFade,
-//   Autoplay,
-//   Navigation,
-//   Pagination,
-// } from "swiper";
-// import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay, EffectFade } from "swiper/modules";
@@ -24,6 +17,7 @@ function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
   useEffect(() => {
     async function fetchListing() {
       const docRef = doc(db, "listings", params.listingId);
@@ -60,6 +54,23 @@ function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center "
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 px-4 py-2">
+          Link Copied
+        </p>
+      )}
     </main>
   );
 }
